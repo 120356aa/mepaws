424c82582eb9e0128a11086b7bdd4401852bcdf7
120356aa <yurlovandrew@gmail.com>
2019-07-24 03:31:17 -0700

    router and basic header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import Header from './components/header/header';
 
 function App() {
   return (
     <div className="App">
-
+      <Route path="/" component={Header} />
     </div>
   );
 }
---
+++ src/components/header/headerStyles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const HeaderWrap = styled.div`
+  width: 100%;
+  border: 2px solid red;
+  padding: 10px 0 10px 0;
+`;
